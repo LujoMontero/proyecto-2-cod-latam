ea114c30ee2bb35be1403e42f909642466a8507a
repoproyec <luisjosemon@gmail.com>
2023-06-28 18:28:03 -0400

    menu desplegable

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("hola")
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', function () {
+    menu.classList.toggle('active');
+});
