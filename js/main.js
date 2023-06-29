9944bb1216e9ca1ec14d5a0696c20730c44765d7
repoproyec <luisjosemon@gmail.com>
2023-06-29 10:51:55 -0400

    botones

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ const menu = document.getElementById('menu');
 menuIcon.addEventListener('click', function () {
     menu.classList.toggle('active');
 });
+
+let parrafoElement = document.getElementById("bio");
+console.log(parrafoElement.innerHTML); 
+
+let botonAgregarClaseEIDOS = document.getElementById("agregar-EIDOS");
+let botonAgregarClaseACCENTURE = document.getElementById("agregar-ACCENTURE");
+let botonQuitarClasefondos = document.getElementById("quitar-fondos");
+
+botonAgregarClaseEIDOS.addEventListener("click", function () {
+    parrafoElement.classList.add("red-title");
+});
+botonAgregarClaseACCENTURE.addEventListener("click", function () {
+    parrafoElement.classList.add("red-title");
+});
+
+botonQuitarClasefondos.addEventListener("click", function () {
+    parrafoElement.classList.remove("red-title");
+});
